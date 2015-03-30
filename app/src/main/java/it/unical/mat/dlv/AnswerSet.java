193fd5b4b2e82f367fc7a55354f53d290fb7ae0c
brainatwork <dario.campisano@gmail.com>
2015-03-30 10:25:32 +0200

    AnswerSet class Modifyed. Added Getter for HashMap weightMap. Added Utility Func. to check if weightMap is an empty HashMap;

--- app/src/main/java/it/unical/mat/dlv/AnswerSet.java
+++ app/src/main/java/it/unical/mat/dlv/AnswerSet.java
@@ -7,22 +7,29 @@ import java.util.HashMap;
  */
 public class AnswerSet {
     private String answerSet;
+    private boolean isMapEmpty;
     private HashMap<Integer,Integer> weightMap;
 
-    public AnswerSet(){
-
+    public AnswerSet(String outputString, HashMap<Integer, Integer> weightMap){
+        this.answerSet = outputString;
+        this.weightMap = weightMap;
+        this.isMapEmpty = weightMap.isEmpty();
     }
-
     public AnswerSet(String outputString){
         this.answerSet = outputString;
+        this.weightMap = new HashMap<Integer, Integer>();
+        this.isMapEmpty = weightMap.isEmpty();
     }
 
     public String getAnswerSet(){
         return this.answerSet;
     }
 
-    public void setAnswerSet(String outputString){
-        this.answerSet = outputString;
+    public HashMap<Integer, Integer> getWeightMap() {
+        return this.weightMap;
     }
 
+    public boolean isWeightMapEmpty(){
+        return this.isMapEmpty;
+    }
 }
