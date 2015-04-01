78853ffe8d9115e9ccd545908450b177d8a5e1ac
brainatwork <dario.campisano@gmail.com>
2015-04-01 12:15:56 +0200

    AnswerSets: methods added.

--- app/src/main/java/it/unical/mat/andlv/base/AnswerSets.java
+++ app/src/main/java/it/unical/mat/andlv/base/AnswerSets.java
@@ -5,9 +5,29 @@ import java.util.ArrayList;
 
 /**
  * Created by Dario Campisano on 01/04/2015.
+ * <p>AnswerSets class store and parse Answer Sets</p>
  */
 public abstract class AnswerSets {
+    private String answerSets; //contains String Answer Sets
+
     private ArrayList<AnswerSet> answerSetsList; //ArrayList of Answer Sets
 
+    /**
+     * Constructor initialize AnswerSets object with a String tha contains ALL Answer Set generated.
+     * @param answerSets
+     */
+    public AnswerSets(String answerSets){
+        this.answerSets = answerSets;
+    }
+
+    /**
+     * Parse outputToParse string and generate AnswerSet objects
+     * @param outputToParse
+     */
     abstract protected void parse(String outputToParse);//Create AnswerSet objects from outputToParse String. Then add objects to answerSetList
+
+    public ArrayList<AnswerSet> getAnswerSetsList() {
+
+        return answerSetsList;
+    }
 }
