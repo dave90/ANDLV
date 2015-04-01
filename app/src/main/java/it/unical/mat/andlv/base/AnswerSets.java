d4a0d48a4127f28fd9c8ba6604f03e83be1e2e98
brainatwork <dario.campisano@gmail.com>
2015-04-01 11:58:36 +0200

    AnswerSets class created. private ArrayList<AnswerSet> added.
     abstract method parse(String) added.

--- app/src/main/java/it/unical/mat/andlv/base/AnswerSets.java
+++ app/src/main/java/it/unical/mat/andlv/base/AnswerSets.java
@@ -1,7 +1,13 @@
 package it.unical.mat.andlv.base;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by Dario Campisano on 01/04/2015.
  */
 public abstract class AnswerSets {
+    private ArrayList<AnswerSet> answerSetsList; //ArrayList of Answer Sets
+
+    abstract protected void parse(String outputToParse);//Create AnswerSet objects from outputToParse String. Then add objects to answerSetList
 }
